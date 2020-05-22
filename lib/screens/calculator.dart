09e5309c82fd4f117c2b0d6ff3faa89b76ebee5d
André Vicente <andre.vnp@live.com>
2020-05-22 10:41:32 -0300

    First Commit

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../components/keyboard.dart';
+import '../components/display.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(children: <Widget>[
+        Display('123.45'),
+        Keyboard(),
+      ],)
+    );
+  }
+}
